0e6c34d08622efb6a96f76ee4cd7082c044998f9
tahmasbi92 <atemaaaa92@gmail.com>
2024-11-22 22:24:01 +0330

    fiber1

--- fiber1.go
+++ fiber1.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	// Fiber instance
+
 	app := fiber.New()
 
 	// Routes
-	app.Get("/", hello)
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"message": "Hello Everything is working perfect ana as expected!",
+			"status":  http.StatusOK,
+		})
+	})
+	app.Listen("3000")
 
-	// Start server
-	log.Fatal(app.Listen(":3001"))
-}
-
-// Handler
-func hello(c *fiber.ctx) error {
-	return c.sendstring("Hello, world!")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
