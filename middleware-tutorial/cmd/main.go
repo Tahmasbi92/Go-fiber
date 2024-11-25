82d83f634adfa4554045f69b5bb5ca327e10ba22
tahmasbi92 <atemaaaa92@gmail.com>
2024-11-25 19:30:52 +0330

    server.go

---
+++ middleware-tutorial/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Tahmasbi92/middleware-tutorial/server"
+
+func main() {
+	server.Run()
+}
---
+++ middleware-tutorial/middleware-tutorial/go.mod
@@ -0,0 +1,3 @@
+module github.com/Tahmasbi92/middleware-tutorial
+
+go 1.23.1
