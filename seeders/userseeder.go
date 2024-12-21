630928e1b1b9cb93874ff710ec8cdad8adad2114
tahmasbi92 <atemaaaa92@gmail.com>
2024-12-21 21:57:27 +0330

    اضافه کردن فایل

---
+++ seeders/userseeder.go
@@ -0,0 +1,63 @@
+package seeders
+
+import (
+	"log"
+
+	db "github.com/Tahmasbi92/go-fiber3/database"
+	"github.com/Tahmasbi92/go-fiber3/models"
+)
+
+func seedusers() {
+
+	dbClient := db.GetDB()
+	// ravesh 1
+	//var user models.use
+	//user.Email = "test@test.com"
+	//user.Username = "test"
+	//user.Password = "123456789"
+
+	//ravesh 2
+	User := models.user{
+		Username : "Atarod61",
+		Password : "123456789",
+		Email : "atarod2017@gmail.com"
+	}
+
+	err := dbClient.Create(&user).Error
+
+	if err != nil {
+		log.Fatalf("user cannot be added to database")
+	}
+
+}package seeders
+
+import (
+	"log"
+
+	db "github.com/Atarod61/go-fiber3/database"
+	"github.com/Atarod61/go-fiber3/models"
+)
+
+func seedusers() {
+
+	dbClient := db.GetDB()
+	// ravesh 1
+	//var user models.use
+	//user.Email = "test@test.com"
+	//user.Username = "test"
+	//user.Password = "123456789"
+
+	//ravesh 2
+	User := models.user{
+		Username : "Atarod61",
+		Password : "123456789",
+		Email : "atarod2017@gmail.com"
+	}
+
+	err := dbClient.Create(&user).Error
+
+	if err != nil {
+		log.Fatalf("user cannot be added to database")
+	}
+
+}
